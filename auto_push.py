9f365b56b24077e55ec51ab68c840cbe37b869cb
alex1109 <1355560728@qq.com>
2020-08-31 14:02:55 +0800

    微博推送

--- auto_push.py
+++ auto_push.py
@@ -13,36 +13,36 @@ import random
 """
 深圳地区和仁爱医院病种
 """
-sz_loc = './blog/weibo/地区/sz_location.txt'
-ra_disease = './blog/weibo/病种/ra_disease_renliu.txt'
+sz_loc = './地区/sz_location.txt'
+ra_disease = './病种/ra_disease_renliu.txt'
 
 """
 厦门地区和鹭港医院病种
 """
 
-xm_loc = './blog/weibo/地区/xm_location.txt'
-lg_disease = './blog/weibo/病种/lg_disease_renliu.txt'
+xm_loc = './地区/xm_location.txt'
+lg_disease = './病种/lg_disease_renliu.txt'
 
 """
 东莞地区和东方医院病种
 """
-dg_loc = './blog/weibo/地区/dg_location.txt'
-df_disease = './blog/weibo/病种/df_disease.txt'
+dg_loc = './地区/dg_location.txt'
+df_disease = './病种/df_disease.txt'
 """
 台州
 """
-tz_loc = './blog/weibo/地区/tz_location.txt'
-tz_disease = './blog/weibo/病种/tz_disease.txt'
+tz_loc = './地区/tz_location.txt'
+tz_disease = './病种/tz_disease.txt'
 """
 宁波
 """
-nb_loc = './blog/weibo/地区/nb_location.txt'
-nb_disease = './blog/weibo/病种/nb_disease.txt'
+nb_loc = './地区/nb_location.txt'
+nb_disease = './病种/nb_disease.txt'
 """
 温州
 """
-wz_loc = './blog/weibo/地区/wz_location.txt'
-wz_disease = './blog/weibo/病种/wz_disease.txt'
+wz_loc = './地区/wz_location.txt'
+wz_disease = './病种/wz_disease.txt'
 """
 账号密码
 """
@@ -50,8 +50,8 @@ sz_account = ''
 """
 修饰词
 """
-compose_key = './blog/weibo/组合词/compose_key.txt'
-with open('./blog/weibo/随机文章内容/rand_content.txt', 'r', encoding='UTF-8') as rand_s:
+compose_key = './组合词/compose_key.txt'
+with open('./随机文章内容/rand_content.txt', 'r', encoding='UTF-8') as rand_s:
     R_string = rand_s.readlines()
     rand_str = random.choice(R_string)
 # rand_string = open('/rand_string.txt', 'r', encoding='UTF-8')
@@ -89,28 +89,30 @@ compose_kw = randString(compose_key)  # 所有关键词修饰定语
 ra_total_key = '['+randString(sz_loc) + \
     randString(ra_disease)+randString(compose_key)+']'
 
-ra_img_dir = './blog/weibo/renai_pic/'
+ra_img_dir = './renai_pic/'
 ra_img_upload = os.listdir(ra_img_dir)
 ra_img_upload.sort(key=lambda i: int(re.match(r'(\d+)', i).group()))
 img = ra_img_upload[:9]
 
-lg_img_dir = './blog/weibo/lugang_pic'
+lg_img_dir = './lugang_pic'
 lg_img_upload = os.listdir(lg_img_dir)
 lg_img_upload.sort(key=lambda i: int(re.match(r'(\d+)', i).group()))
 img = lg_img_upload[:9]
 
 main_dir = os.getcwd()
 
-ra_upload_img_dir = '\\blog\\weibo\\renai_pic'
-lg_upload_img_dir = '\\blog\\weibo\\lugang_pic'
+ra_upload_img_dir = '\\renai_pic'
+lg_upload_img_dir = '\\lugang_pic'
 
 with open('./账号/ra_account.txt', 'r', encoding='utf-8-sig') as ra_account:  # 深圳账号
     shenzhen_account = ra_account.readlines()
-sz_account = shenzhen_account
+    sz_account = shenzhen_account
+    ra_account.close()
 
 with open('./账号/lg_account.txt', 'r', encoding='utf-8-sig') as lg_account:  # 厦门账号
-    lugang_account = ra_account.readlines()
-xm_account = lugang_account
+    lugang_account = lg_account.readlines()
+    xm_account = lugang_account
+    lg_account.close()
 
 ra_post_time = 0
 lg_post_time = 0
@@ -153,8 +155,8 @@ if len(sz_account) % 4 == 0:
     while len(sz_account) >= 4:
         login(sz_account[-4], sz_account[-3])
         sleep(8)
-        while WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-                                                                           ".func a[node-type='submit']"))) ra_post_time <= sz_account[-1]:  # 文章发布次数
+        # 文章发布次数
+        while WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".func a[node-type='submit']"))) and ra_post_time <= sz_account[-1]:
             sleep(2)
             upload = browser.find_element_by_css_selector(
                 'form[id="pic_upload"]').click()
@@ -193,60 +195,60 @@ if len(sz_account) % 4 == 0:
                 ".func a[node-type='submit']").click()
             sleep(3)
             ra_post_time += 1
-        logout()
     sz_account.pop()
     sz_account.pop()
     sz_account.pop()
     sz_account.pop()
+    logout()
 
-elif len(xm_account) % 4 == 0:
-    while len(xm_account) >= 4:
-        login(xm_account[-4], xm_account[-3])
-        sleep(8)
-        while WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-                                                                           ".func a[node-type='submit']"))) lg_post_time <= xm_account[-1]:  # 文章发布次数
-            sleep(2)
-            upload = browser.find_element_by_css_selector(
-                'form[id="pic_upload"]').click()
-            for up_img in img:
-                imgFile = main_dir + lg_upload_img_dir + '\\' + up_img  # 文件图片绝对路径
-                dialog = win32gui.FindWindow('#32770', "文件上传")  # 一级
-                ComboBoxEx32 = win32gui.FindWindowEx(
-                    dialog, 0, 'ComboBoxEx32', None)  # 二级
-                ComboBox = win32gui.FindWindowEx(
-                    ComboBoxEx32, 0, 'ComboBox', None)  # 三级
-                edit = win32gui.FindWindowEx(ComboBox, 0, 'Edit', None)  # 四级
-                button = win32gui.FindWindowEx(dialog, 0, 'Button', None)  # 五级
-                win32gui.SendMessage(edit, win32con.WM_SETTEXT,
-                                     None, imgFile)  # 循环到最后一次退出上传框
-                win32gui.SendMessage(dialog, win32con.WM_COMMAND,
-                                     1, button)  # 循环到最后一次退出上传框
-                if up_img not in img[len(img)-1]:
-                    WebDriverWait(browser, 15).until(
-                        EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'ul>li[class="add"]')))
-                    browser.find_element_by_css_selector(
-                        'ul>li[class="add"]').click()
-            sleep(3)
-            lg_total_key1 = '['+randString(lg_loc) + \
-                randString(lg_disease)+randString(compose_key)+']'
-            lg_total_key2 = '['+randString(lg_loc) + \
-                randString(lg_disease)+randString(compose_key)+']'
-            lg_total_key3 = '['+randString(lg_loc) + \
-                randString(lg_disease)+randString(compose_key)+']'
+# if len(xm_account) % 4 == 0:
+#     while len(xm_account) >= 4:
+#         login(xm_account[-4], xm_account[-3])
+#         sleep(8)
+#         # 文章发布次数
+#         while WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".func a[node-type='submit']"))) and lg_post_time <= xm_account[-1]:
+#             sleep(2)
+#             upload = browser.find_element_by_css_selector(
+#                 'form[id="pic_upload"]').click()
+#             for up_img in img:
+#                 imgFile = main_dir + lg_upload_img_dir + '\\' + up_img  # 文件图片绝对路径
+#                 dialog = win32gui.FindWindow('#32770', "文件上传")  # 一级
+#                 ComboBoxEx32 = win32gui.FindWindowEx(
+#                     dialog, 0, 'ComboBoxEx32', None)  # 二级
+#                 ComboBox = win32gui.FindWindowEx(
+#                     ComboBoxEx32, 0, 'ComboBox', None)  # 三级
+#                 edit = win32gui.FindWindowEx(ComboBox, 0, 'Edit', None)  # 四级
+#                 button = win32gui.FindWindowEx(dialog, 0, 'Button', None)  # 五级
+#                 win32gui.SendMessage(edit, win32con.WM_SETTEXT,
+#                                     None, imgFile)  # 循环到最后一次退出上传框
+#                 win32gui.SendMessage(dialog, win32con.WM_COMMAND,
+#                                     1, button)  # 循环到最后一次退出上传框
+#                 if up_img not in img[len(img)-1]:
+#                     WebDriverWait(browser, 15).until(
+#                         EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'ul>li[class="add"]')))
+#                     browser.find_element_by_css_selector(
+#                         'ul>li[class="add"]').click()
+#             sleep(3)
+#             lg_total_key1 = '['+randString(lg_loc) + \
+#                 randString(lg_disease)+randString(compose_key)+']'
+#             lg_total_key2 = '['+randString(lg_loc) + \
+#                 randString(lg_disease)+randString(compose_key)+']'
+#             lg_total_key3 = '['+randString(lg_loc) + \
+#                 randString(lg_disease)+randString(compose_key)+']'
 
-            lg_content = lg_total_key1+lg_total_key2+'欢迎拨打下方咨询热线' + \
-                lg_total_key+rand_str+lg_total_key3  # 动态调用
+#             lg_content = lg_total_key1+lg_total_key2+'欢迎拨打下方咨询热线' + \
+#                 lg_total_key+rand_str+lg_total_key3  # 动态调用
 
-            browser.find_element_by_css_selector(
-                ".input textarea[node-type='textEl']").send_keys(ra_content)
-            browser.find_element_by_css_selector(
-                ".func a[node-type='submit']").click()
-            sleep(3)
-            lg_post_time += 1
-        logout()
-    lg_account.pop()
-    lg_account.pop()
-    lg_account.pop()
-    lg_account.pop()
+#             browser.find_element_by_css_selector(
+#                 ".input textarea[node-type='textEl']").send_keys(ra_content)
+#             browser.find_element_by_css_selector(
+#                 ".func a[node-type='submit']").click()
+#             sleep(3)
+#             lg_post_time += 1
+#     lg_account.pop()
+#     lg_account.pop()
+#     lg_account.pop()
+#     lg_account.pop()
+#     logout()
 
 browser.close()
